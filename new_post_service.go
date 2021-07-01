fd5e5bd4ac18c7bc22084bd7350243234b42d14d
maxkucher <maxkucher1998@gmail.com>
2021-07-01 17:43:59 -0400

    Move new post service to separate file

---
+++ new_post_service.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type InternetDocument struct {
+	ReferenceId           string `json:"Ref"`
+	RecipientContactPhone string `json:"RecipientContactPhone"`
+	DeliveryStatus        string `json:"StateId"`
+}
+
+type NewPostRequest struct {
+	ModelName        string            `json:"modelName"`
+	CalledMethod     string            `json:"calledMethod"`
+	MethodProperties map[string]string `json:"methodProperties"`
+	ApiKey           string            `json:"apiKey"`
+}
+
+type NewPostService interface {
+	GetAllInternetDocuments() ([]InternetDocument, error)
+	FilterInternetDocumentsAwaitingPickup([]InternetDocument) []InternetDocument
+}
+
+type GetInternetDocumentsResponse struct {
+	Success bool               `json:"success"`
+	Data    []InternetDocument `json:"data"`
+	Errors  []string           `json:"errors"`
+}
+
+type NewPostServiceImpl struct {
+	apiKey string
+}
+
+func (n NewPostServiceImpl) GetAllInternetDocuments() ([]InternetDocument, error) {
+	requestUrl := "https://api.novaposhta.ua/v2.0/json/"
+	requestBody := NewPostRequest{
+		ModelName:    "InternetDocument",
+		CalledMethod: "getDocumentList",
+		MethodProperties: map[string]string{
+			"GetFullList":  "1",
+			"DateTimeFrom": "01.01.2021",
+			"DateTimeTo":   "01.01.2050",
+		},
+		ApiKey: n.apiKey,
+	}
+	requestBodyBytes, _ := json.Marshal(requestBody)
+	resp, apiError := http.Post(requestUrl, "application/json", bytes.NewReader(requestBodyBytes))
+	if apiError != nil {
+		return nil, apiError
+	}
+	var response GetInternetDocumentsResponse
+	jsonError := json.NewDecoder(resp.Body).Decode(&response)
+	if jsonError != nil {
+		return nil, jsonError
+	}
+	if response.Success == true {
+		return response.Data, nil
+	} else {
+		return nil, errors.New("failed to fetch internet documents")
+	}
+}
+
+func (n NewPostServiceImpl) FilterInternetDocumentsAwaitingPickup(documents []InternetDocument) []InternetDocument {
+	var awaitingPickup []InternetDocument
+	for currentIndex := range documents {
+		document := documents[currentIndex]
+		documentDeliveryStatus := document.DeliveryStatus
+		if documentDeliveryStatus == "7" || documentDeliveryStatus == "8" {
+			awaitingPickup = append(awaitingPickup, document)
+		}
+	}
+	return awaitingPickup
+}
