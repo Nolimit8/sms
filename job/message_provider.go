c480cca64cc60e9b8a55489d2db148462305c5d9
maxkucher <maxkucher1998@gmail.com>
2021-07-05 17:17:14 -0400

    Fix day interval calculation

--- job/message_provider.go
+++ job/message_provider.go
@@ -3,6 +3,7 @@ package job
 import (
 	"bytes"
 	"errors"
+	"math"
 	"text/template"
 	"time"
 )
@@ -18,7 +19,7 @@ type DaysAwareMessageProvider struct {
 func (provider DaysAwareMessageProvider) GetMessageForInternetDocumentsAwaitingPickup(document InternetDocument) (string, error) {
 	statusUpdateDate, _ := time.Parse("2006-01-02 15:04:05", document.StatusUpdateDate)
 	currentDateTime := time.Now()
-	timeFromLastStatusUpdate := int(currentDateTime.Sub(statusUpdateDate).Hours() / 24)
+	timeFromLastStatusUpdate := int(math.Floor(currentDateTime.Sub(statusUpdateDate).Hours() / 24))
 	if timeFromLastStatusUpdate >= 0 && timeFromLastStatusUpdate <= 8 {
 		t, _ := template.New("smsTemplate").Parse(DocumentsAwaitingPickupSMSTemplate(timeFromLastStatusUpdate).GetTemplate())
 		var tpl bytes.Buffer
